2ee04731a08057d01df3b532c9e442dd9db5a3a4
SiaVash <picinfiniti@gmail.com>
2019-05-25 20:54:01 +0430

    calculate bey for each namad

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import jdatetime as jdt
+from DataBase import *
 from Input import Input
+import numpy as np
 
-def BEY(PV, SR, FV=1000000):
-    return (FV/PV-1)*(365/SR)
+#{Code:[SR, Hajm, Close, Payani]}
 
 with open("namad.csv") as namads:
     Namads = {}
@@ -10,5 +11,33 @@ with open("namad.csv") as namads:
         hv = i.split(",")#help variable
         today = jdt.datetime.now().date()
         Namads[hv[0]] = [(jdt.datetime.strptime(hv[1],"%Y.%m.%d").date()-today).days]
+
+def BEY(PV, SR, FV=1000000):
+    return (FV/PV-1)*(365/SR)
+
+DB = 'StockData.db'
+conn = sqlite3.connect(DB)
+cursor = conn.cursor()
+
+        # extract all stock name stored in database ans store to List
+cursor.execute('''
+                SELECT
+                    Code, Hajm, Close, Payani
+                FROM
+                    StockInfo
+                WHERE
+                    Code in Namad'''.replace("Namad",str(tuple(Namads.keys()))))
+List = cursor.fetchall()
+
+#add BEY
+for i in List :
+    SR = Namads[i[0]][0]
+    PV = int([i[2],i[3]][int(i[1])==0])
+    if int(i[1]):
+        Namads[i[0]].append(BEY(PV, SR))
+        
+
+
+
 
 
