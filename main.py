7e0bb1f162308cdd450f2a7d9a1e9aceb9b3cf65
SiaVash <picinfiniti@gmail.com>
2019-05-25 21:07:42 +0430

    add infinite loop

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import jdatetime as jdt
 from DataBase import *
 from Input import Input
+from time import sleep
 import numpy as np
-
 #{Code:[SR, Hajm, Close, Payani]}
 
 with open("namad.csv") as namads:
@@ -10,33 +10,35 @@ with open("namad.csv") as namads:
     for i in namads.read().split():
         hv = i.split(",")#help variable
         today = jdt.datetime.now().date()
-        Namads[hv[0]] = [(jdt.datetime.strptime(hv[1],"%Y.%m.%d").date()-today).days]
+        Namads[hv[0]] = [(jdt.datetime.strptime(hv[1],"%Y.%m.%d").date()-today).days, 0]
 
 def BEY(PV, SR, FV=1000000):
     return (FV/PV-1)*(365/SR)
 
 DB = 'StockData.db'
-conn = sqlite3.connect(DB)
-cursor = conn.cursor()
 
-        # extract all stock name stored in database ans store to List
-cursor.execute('''
-                SELECT
-                    Code, Hajm, Close, Payani
-                FROM
-                    StockInfo
-                WHERE
-                    Code in Namad'''.replace("Namad",str(tuple(Namads.keys()))))
-List = cursor.fetchall()
-
-#add BEY
-for i in List :
-    SR = Namads[i[0]][0]
-    PV = int([i[2],i[3]][int(i[1])==0])
-    if int(i[1]):
-        Namads[i[0]].append(BEY(PV, SR))
-        
-
+while True:
+    sleep(1)
+    UpdateDB()
+    conn = sqlite3.connect(DB)
+    cursor = conn.cursor()
+    # extract all stock name stored in database ans store to List
+    cursor.execute('''
+                    SELECT
+                        Code, Hajm, Close, Payani
+                    FROM
+                        StockInfo
+                    WHERE
+                        Code in Namad'''.replace("Namad",str(tuple(Namads.keys()))))
+    List = np.array(cursor.fetchall())
+    conn.close()
+    print(List,"\n")
+    #add BEY
+    for i in List :
+        SR = Namads[i[0]][0]
+        PV = int([i[2],i[3]][int(i[1])==0])
+        if int(i[1]):
+            Namads[i[0]][1] = BEY(PV, SR)
 
 
 
