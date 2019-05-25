3116b477a78948f68d5c9da184467f7805a79906
SiaVash <picinfiniti@gmail.com>
2019-05-25 20:02:35 +0430

    add BEY

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import jdatetime as jdt
 from Input import Input
 
-def BEY():
-    
+def BEY(PV, SR, FV=1000000):
+    return (FV/PV-1)*(365/SR)
+
+with open("namad.csv") as namads:
+    Namads = {}
+    for i in namads.read().split():
+        hv = i.split(",")#help variable
+        today = jdt.datetime.now().date()
+        Namads[hv[0]] = [(jdt.datetime.strptime(hv[1],"%Y.%m.%d").date()-today).days]
+
+
